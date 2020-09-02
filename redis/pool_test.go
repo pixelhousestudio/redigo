3a14a7b90625dd947d0648a8255cbf4e79d0fd2b
Muhammad Faris <muhfaris@disroot.org>
2020-09-02 22:04:28 +0700

    refactor(chore): update package to muhfaris

--- redis/pool_test.go
+++ redis/pool_test.go
@@ -24,7 +24,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gomodule/redigo/redis"
+	"github.com/muhfaris/redigo/redis"
 )
 
 const (
--- redis/scan_test.go
+++ redis/scan_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/gomodule/redigo/redis"
+	"github.com/muhfaris/redigo/redis"
 	"github.com/stretchr/testify/require"
 )
 
